119da2741b9bb320809a20c4d23fa5534b10ef28
Felix <felixcesar2078@gmail.com>
2024-08-20 10:07:52 -0500

    cambios

---
+++ seminario/src/components/login.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+// rcc importante
+
+class Login extends Component {
+    render() {
+        return (
+        );
+    }
+}
+
+Login.propTypes = {
+
+};
+
+
+export default Login;
